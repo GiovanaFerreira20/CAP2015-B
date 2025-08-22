5c3c9d0b19c52bb9323011199400f034901fc4f9
GiovanaFerreira20 <270983@unifio.edu.br>
2025-08-22 01:33:26 +0000

    Gradle

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -2,6 +2,13 @@ package app;
 
 public class PrimeiraClasse {
     public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+        a1.setNome("Giovana");
+        a1.setIdade(24);       
+
+        System.out.println(a1.getNome());
+        System.out.println(a1.getIdade());
+
         System.out.println("Olá Gradle!!!");
     }
 }
